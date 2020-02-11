b6c9129541d338ed9873dca525b970e0b326ac57
HadesZ <bruce_zxy@163.com>
2020-02-11 22:32:25 +0800

    feat: 🎸 将数据纬度从2048个改为1024个

--- index.py
+++ index.py
@@ -9,10 +9,55 @@ import h5py
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(BASE_DIR)
 
+# def getDataFiles(list_filename):
+#     return [line.rstrip() for line in open(list_filename)]
+
+
+# def loadDataFile(path):
+#     data = np.loadtxt(path)
+#     num = data.shape[0]
+#     point_xyz = data[:, 0:3]
+#     point_normal = data[:, 3:6]
+#     point_rgb = data[:, 6:9]
+#     # label just a example, should be repalced the real.
+#     # modlenet40 is 0-39, so the label can be 40 and 41
+#     label = np.ones((num, 1), dtype=int)+39
+#     return point_xyz, label
+
+
+# def change_scale(data):
+#     #centre
+#     xyz_min = np.min(data[:, 0:3], axis=0)
+#     xyz_max = np.max(data[:, 0:3], axis=0)
+#     xyz_move = xyz_min+(xyz_max-xyz_min)/2
+#     data[:, 0:3] = data[:, 0:3]-xyz_move
+#     #scale
+#     scale = np.max(data[:, 0:3])
+#     data[:, 0:3] = data[:, 0:3]/scale
+#     return data
+
+
+# def sample_data(data, num_sample):
+#     """ data is in N x ...
+#         we want to keep num_samplexC of them.
+#         if N > num_sample, we will randomly keep num_sample of them.
+#         if N < num_sample, we will randomly duplicate samples.
+#     """
+#     N = data.shape[0]
+#     if (N == num_sample):
+#         return data, range(N)
+#     elif (N > num_sample):
+#         sample = np.random.choice(N, num_sample)
+#         return data[sample, ...], sample
+#     else:
+#         sample = np.random.choice(N, num_sample-N)
+#         dup_data = data[sample, ...]
+#         return np.concatenate([data, dup_data], 0), list(range(N))+list(sample)
+
 
 def get_csv_list(path):
     # 获取csv文件列表
-    csv_file_list = [[],[],[]]
+    csv_file_list = [[], [], []]
     file_list = os.listdir(path)
     for file_name in file_list:
         if file_name.endswith('csv'):
@@ -21,55 +66,9 @@ def get_csv_list(path):
             csv_file_list[csv_type].append(path + file_name)
     return csv_file_list
 
-def getDataFiles(list_filename):
-    return [line.rstrip() for line in open(list_filename)]
-
-
-def loadDataFile(path):
-    data = np.loadtxt(path)
-    num = data.shape[0]
-    point_xyz = data[:, 0:3]
-    point_normal = data[:, 3:6]
-    point_rgb = data[:, 6:9]
-    # label just a example, should be repalced the real.
-    # modlenet40 is 0-39, so the label can be 40 and 41
-    label = np.ones((num, 1), dtype=int)+39
-    return point_xyz, label
-
-
-def change_scale(data):
-    #centre
-    xyz_min = np.min(data[:, 0:3], axis=0)
-    xyz_max = np.max(data[:, 0:3], axis=0)
-    xyz_move = xyz_min+(xyz_max-xyz_min)/2
-    data[:, 0:3] = data[:, 0:3]-xyz_move
-    #scale
-    scale = np.max(data[:, 0:3])
-    data[:, 0:3] = data[:, 0:3]/scale
-    return data
-
-
-def sample_data(data, num_sample):
-    """ data is in N x ...
-        we want to keep num_samplexC of them.
-        if N > num_sample, we will randomly keep num_sample of them.
-        if N < num_sample, we will randomly duplicate samples.
-    """
-    N = data.shape[0]
-    if (N == num_sample):
-        return data, range(N)
-    elif (N > num_sample):
-        sample = np.random.choice(N, num_sample)
-        return data[sample, ...], sample
-    else:
-        sample = np.random.choice(N, num_sample-N)
-        dup_data = data[sample, ...]
-        return np.concatenate([data, dup_data], 0), list(range(N))+list(sample)
-
-
 def get_csv_data(path_list_arr):
     # 创建空的定维数组
-    sum_data = np.empty([0, 2048, 3], dtype=float)
+    sum_data = np.empty([0, 1024, 3], dtype=float)
     type_data = np.empty([0, 1], dtype=int)
     # 类型序号
     type_serial = 0
@@ -82,8 +81,8 @@ def get_csv_data(path_list_arr):
             # 将每个csv文件读取为Numpy的数据
             data = np.genfromtxt(path, delimiter=',',dtype=None)
             # 计算空值补缺的数量
-            empty_len = 2048 - len(data)
-            # 完整的2048个元数据=csv文件数据+空值数据
+            empty_len = 1024 - len(data)
+            # 完整的1024个元数据=csv文件数据+空值数据
             data_full = np.append(data, np.empty([empty_len, 3], dtype=float), axis=0)
             # 数据归并
             sum_data = np.append(sum_data, [data_full], axis=0)
@@ -104,14 +103,14 @@ if __name__ == "__main__":
     open("plant_train.h5", 'w')
     with h5py.File('plant_train.h5', 'r+') as f:
         f.create_dataset('data', data=train_data)
-        f.create_dataset('faceId', data=np.empty([2048,2048], dtype=float))
+        f.create_dataset('faceId', data=np.empty([1024,1024], dtype=float))
         f.create_dataset('label', data=train_type_data)
         f.create_dataset('normal', data=train_data)
 
     open("plant_test.h5", 'w')
     with h5py.File('plant_test.h5', 'r+') as f:
         f.create_dataset('data', data=test_data)
-        f.create_dataset('faceId', data=np.empty([2048,2048], dtype=float))
+        f.create_dataset('faceId', data=np.empty([1024,1024], dtype=float))
         f.create_dataset('label', data=test_type_data)
         f.create_dataset('normal', data=test_data)
 
